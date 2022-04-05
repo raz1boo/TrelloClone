a307ec915ff33eeaffbd22599bc99f5ec0498509
raz1boo <pavlik.shevchenko96@gmail.com>
2022-04-05 23:51:10 +0300

    task_1_Pavel

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,27 @@
 // верстка
+document.getElementById('root').innerHTML = `
+<main>
+<div class="wrapper">
+    <div class="list-wrapper todo">
+        <div class="head-box">
+            <p class="head-box-description">ToDo</p>
+        </div>
+        <div class="footer-box__add"></div>
+        <div class="add-todo-button">+</div>
+    </div>
+    <div class="list-wrapper inprogress">
+        <div class="head-box">
+            <p class="head-box-description">In Progress</p>
+        </div>
+        <div class="footer-box__inprogress"></div>
+    </div>
+    <div class="list-wrapper done">
+        <div class="head-box">
+            <p class="head-box-description">Done</p>
+            <div class="delete-all-button"></div>
+        </div>
+        <div class="footer-box__done"></div>
+    </div>
+</div>
+</main>
+`
