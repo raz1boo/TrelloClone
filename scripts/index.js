a8514d8627394929a29663fd53280a4f59aa9366
raz1boo <pavlik.shevchenko96@gmail.com>
2022-04-17 23:55:02 +0300

    2.5v

--- scripts/index.js
+++ scripts/index.js
@@ -1,83 +1,103 @@
 // верстка
-document.getElementById('root').innerHTML = `
+document.body.innerHTML = `
+<header>
+<a href="#" class="logo"></a>
+<div class="header_tools">
+    <input type="text" placeholder="Search..">
+    <button><img src="../assets/icons/gear.png"></button>
+</div>
+</header>
 <main>
 <div class="wrapper">
     <div class="list-wrapper todo">
-        <div class="head-box">
-            <p class="head-box-description">ToDo</p>
-        </div>
-        <div id="footer-box__add"></div>
+        <div class="head-box">ToDo</div>
         <button id="add-todo-button">✚</button>
         <div id="description-box">
-        <input type="text" placeholder="Title.." id="title-input" maxlength='35'>
-        <textarea type="text" id="todo-description" placeholder="Description.."></textarea>
-        <select name="users" id="user-select"></select>
-        <button id="confirm-form">✚</button>
-        <button id="close-form">✖</button>
+            <select id='point-select' class='point' name='points'>
+                <option value="#0079bf" class='blue'></option>
+                <option value="#f2d600" class='yellow'></option>
+                <option value="#61bd4f" class='green'></option>
+                <option value="#eb5a46" class='red'></option>
+            </select>
+            <input type="text" placeholder="Title..." id="description_title_input" maxlength='25'>
+            <textarea type="text" id="todo-description" placeholder="Description..." maxlength='1000'></textarea>
+            <select name="users" id="user-select" class="user"></select>
+            <button id="confirm-form">✚</button>
+            <button id="close-form">✖</button>
         </div>
+        <div id="add" class='footer-box'></div>
     </div>
     <div class="list-wrapper inprogress">
-        <div class="head-box">
-            <p class="head-box-description">In Progress</p>
-        </div>
-        <div class="footer-box__inprogress"></div>
+        <div class="head-box">In Progress</div>
+        <div class="footer-box" id='inprogress'></div>
     </div>
     <div class="list-wrapper done">
-        <div class="head-box">
-            <p class="head-box-description">Done</p>
-            <button id="delete-all-button"></button>
+        <div class="head-box">Done<button id="delete-all-button"><img src="../assets/icons/delete.png"></button>
         </div>
-        <div class="footer-box__done"></div>
+        <div class="footer-box" id='done'></div>
     </div>
 </div>
 </main>
 <div id="quick-card-editor">
-    <div id='quick-card-editor-close-card'></div>
-    <div id="quick-card-editor-card">
-        <div id="quick-card-newCard">
-            <input class="title-description quick-card-title" id='title-description'maxlength='35' autocomplete="off"></input>
-            <textarea type='text' class='todo-value quick-card-text' id='card-text'></textarea>
-            <div class='card-footer'>
-                <select id='quick-card-user'></select>
-                 <div id='quick-card-date'></div>
-        </div></div>
+<div id='quick-card-editor-close-card'></div>
+<div id="quick-card-editor-card">
+    <div id="quick-card-newCard">
+        <select id='quick-card-point'>
+            <option value="#0079bf" class='blue'></option>
+            <option value="#f2d600" class='yellow'></option>
+            <option value="#61bd4f" class='green'></option>
+            <option value="#eb5a46" class='red'></option>
+        </select>
+        <input class="quick-card-title" id='title-description' maxlength='25'
+            autocomplete="off"></input>
+        <textarea type='text' class='todo-value quick-card-text' id='card-text' maxlength='1000'></textarea>
+        <div class='card-footer'>
+            <select id='quick-card-user'></select>
+            <div id='quick-card-date'></div>
+        </div>
     </div>
-    <span id="quick-card-editor-close-icon"></span>
+</div>
+<span id="quick-card-editor-close-icon"></span>
 </div>
 `
 // объявляем переменные
 const input = document.getElementById('todo-description');
-const titleText = document.getElementById('title-input');
-const cardFooter = document.getElementById('footer-box__add');
+const titleText = document.getElementById('description_title_input');
+const addFooter = document.getElementById('add');
+const inprogressFooter = document.getElementById('inprogress');
+const doneFooter = document.getElementById('done');
 let todoItemsElems = [];
 // Генерация даты
 let date = `${new Date().getDate()}.${new Date().getMonth() + 1}.${new Date().getFullYear()}`;
 // Объявление переменных для localStorage
 let cardArray = JSON.parse(localStorage.getItem('cardArray')) || [];
 let id = JSON.parse(localStorage.getItem('id')) || 0;
-// Создание объекта с разными нужными значениями
-function todo(val, id, tit, us) {
+// Создание объекта с нужными значениями
+function todo(val, id, tit, us, point) {
     this.id = id;
     this.date = date;
     this.text = val;
     this.title = tit;
     this.user = us;
+    this.point = point;
+    this.position = 'add';
 }
 // Создает блок и задает значения для него и для дочерних элементов
 const createTemplate = (inp, index) => {
     return `
-    <div class='newCard'>
+    <div class='newCard' id='newCard-${index}' draggable="true" >
+        <div id='point-${index}' class='point' style='background-color:${inp.point}'></div>
         <div class='card-header'>
             <div class="title-description">${inp.title}</div>
             <div class='title-header-buttons'>
-                <button onclick='editTask(${index})' class='button' id='edit-button'></button>
+                <button onclick='editTask(${index})' class='button'><img src='../assets/icons/edit.png'></button>
                 <button onclick='deleteTask(${index})' class='button'>✖</button>
             </div>
         </div>
         <div class='todo-value'>${inp.text}</div>
         <div class='card-footer'>
-            <div id="user">${inp.user}</div>
-            <div id='date'>${inp.date}</div>
+            <div id="user-${index}" class='card-user'>${inp.user}</div>
+            <div id='date-${index}' class='card-date'>${inp.date}</div>
         </div>
     </div>
 `
@@ -89,41 +109,46 @@ const localUpdate = () => {
     // counters();
 }
 function printUsers(json) {
-    console.log(json);
     json.forEach(i => {
         document.getElementById('user-select').innerHTML += `<option>${i.name}</option>`
         document.getElementById('quick-card-user').innerHTML += `<option>${i.name}</option>`
     })
 }
 function getUsers() {
-    fetch(`https://jsonplaceholder.typicode.com/users/`)
+    fetch('https://jsonplaceholder.typicode.com/users/')
         .then(response => response.json())
         .then(json => { printUsers(json) })
 }
 // Функция сохраняет значения в localStorage и задает текст блоков(при перезагрузке страницы и не только)
 // проверяет наличие 'блока' в массиве и выводит его(заново прорисовывает все блоки из массива, при удалении, при добавлении)
 const htmlText = () => {
-    cardFooter.innerText = '';
+    addFooter.innerText = '';
+    inprogressFooter.innerText = '';
+    doneFooter.innerText = '';
     if (cardArray.length > 0) {
         cardArray.forEach((item, index) => {
-            cardFooter.innerHTML += createTemplate(item, index);
+            if (cardArray[index].position == 'add') addFooter.innerHTML += createTemplate(item, index);
+            if (cardArray[index].position == 'inprogress') inprogressFooter.innerHTML += createTemplate(item, index);
+            if (cardArray[index].position == 'done') doneFooter.innerHTML += createTemplate(item, index);
         });
         todoItemsElems = document.querySelectorAll('.newCard');
     }
-    localUpdate()
+    dragNdrop();
+    localUpdate();
 }
+dragNdrop();
 htmlText()
 getUsers()
 // Функция проверяет наличие текста в инпуте, далее записывает значение в объект из которого берется значение
 // обнуляет инпут
 document.getElementById('confirm-form').addEventListener('click', () => {
     if (input.value == '' || titleText.value == '') {
-        input.placeholder = 'Write a description..'
-        titleText.placeholder = 'Write a title..'
+        input.placeholder = 'Write a description..';
+        titleText.placeholder = 'Write a title..';
         return
     } else {
         id++
-        cardArray.push(new todo(input.value, id, titleText.value, document.getElementById('user-select').value));
+        cardArray.push(new todo(input.value, id, titleText.value, document.getElementById('user-select').value, document.getElementById('point-select').value));
         input.value = '';
         input.placeholder = 'Description..'
         titleText.value = '';
@@ -160,7 +185,7 @@ function getCoords(elem) {
     };
 }
 const editTask = index => {
-    document.getElementById('quick-card-editor-card').style.top = `${getCoords(todoItemsElems[index]).top - 7}px`;
+    document.getElementById('quick-card-editor-card').style.top = `${getCoords(todoItemsElems[index]).top - 15}px`;
     document.getElementById('quick-card-editor-card').style.left = `${getCoords(todoItemsElems[index]).left}px`;
     if (document.getElementById('card-editor-button-confirm')) {
         document.getElementById('card-editor-button-confirm').remove()
@@ -171,21 +196,20 @@ const editTask = index => {
     document.getElementById('quick-card-user').value = `${cardArray[index].user}`;
     document.getElementById('quick-card-date').innerText = `${cardArray[index].date}`;
     document.getElementById('quick-card-newCard').style.boxShadow = 'none';
-    document.getElementById('card-text').style.height = `${this.scrollHeight}px`;
-    console.log(this.scrollHeight);
-    document.getElementById('card-text').addEventListener('input', function () {
-        this.style.height = `${this.scrollHeight}px`;
-        console.log(this.scrollHeight);
+    document.getElementById('quick-card-point').style.backgroundColor = `${cardArray[index].point}`;
+    document.getElementById('quick-card-point').addEventListener('change', function () {
+        if (this.value == '#f2d600') { this.style.cssText = 'background-color: #f2d600;' }
+        else if (this.value == '#0079bf') { this.style.cssText = 'background-color: #0079bf;' }
+        else if (this.value == '#eb5a46') { this.style.cssText = 'background-color: #eb5a46;' }
+        else if (this.value == '#61bd4f') { this.style.cssText = 'background-color: #61bd4f;' }
     })
     document.getElementById('quick-card-editor').style.display = 'block';
 }
 // закрывает окно редактирования
 document.getElementById('quick-card-editor-close-icon').addEventListener('click', () => {
-    document.getElementById('card-text').style.height = `160px`;
     document.getElementById('quick-card-editor').style.display = 'none';
 })
 document.getElementById('quick-card-editor-close-card').addEventListener('click', () => {
-    document.getElementById('card-text').style.height = `160px`;
     document.getElementById('quick-card-editor').style.display = 'none';
 })
 // вписывает в массив новые данные и закрывает окно редактирования
@@ -193,11 +217,82 @@ const confirmChangesCard = index => {
     if (document.getElementById(`title-description`).value && document.getElementById(`card-text`).value) {
         cardArray[index].title = document.getElementById(`title-description`).value;
         cardArray[index].text = document.getElementById(`card-text`).value;
-        cardArray[index].user = document.getElementById('quick-card-user').value
-        document.getElementById('quick-card-editor').style.display = 'none'
-        htmlText()
+        cardArray[index].user = document.getElementById('quick-card-user').value;
+        cardArray[index].point = document.getElementById('quick-card-point').value;
+        document.getElementById('quick-card-editor').style.display = 'none';
+        htmlText();
     }
 }
+document.getElementById('point-select').addEventListener('change', function () {
+    if (this.value == '#f2d600') { this.style.cssText = 'background-color: #f2d600;' }
+    else if (this.value == '#0079bf') { this.style.cssText = 'background-color: #0079bf;' }
+    else if (this.value == '#eb5a46') { this.style.cssText = 'background-color: #eb5a46;' }
+    else if (this.value == '#61bd4f') { this.style.cssText = 'background-color: #61bd4f;' }
+})
+
+let draggedItem = null;
+function dragNdrop() {
+    const items = document.querySelectorAll('.newCard');
+    const lists = document.querySelectorAll('.footer-box');
+    for (let i = 0; i < items.length; i++) {
+        const item = items[i];
+        item.addEventListener('mousedown', () => {
+            draggedItem = item;
+        })
+        item.addEventListener('mouseup', () => {
+            draggedItem = null;
+        })
+        for (let l = 0; l < lists.length; l++) {
+            const list = lists[l];
+            list.addEventListener('dragover', e => e.preventDefault())
+            list.addEventListener('dragenter', function (e) {
+                e.preventDefault();
+                this.style.borderColor = '#091e4214';
+            })
+            list.addEventListener('dragleave', function (e) {
+                e.preventDefault();
+                this.style.borderColor = 'transparent';
+            })
+            list.addEventListener('drop', function (e) {
+                e.preventDefault();
+                this.style.borderColor = 'transparent';
+                this.append(draggedItem);
+                if (this.id == 'inprogress') {
+                    if (item === draggedItem) {
+                        cardArray[i].position = `${this.id}`;
+                        localUpdate();
+                        return
+                    }
+                }
+                if (this.id == 'done') {
+                    if (item === draggedItem) {
+                        cardArray[i].position = `${this.id}`;
+                        localUpdate();
+                        return
+                    }
+                }
+                if (this.id == 'add') {
+                    if (item === draggedItem) {
+                        cardArray[i].position = `${this.id}`;
+                        localUpdate();
+                        return
+                    }
+                }
+            })
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
 // удаление всех блоков, очищение массива
 // document.getElementById('delete-all-button').addEventListener('click', () => {
 //     cardArray = [];
